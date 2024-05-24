b5bdab6b082fce4a05cf7faeb0803237c795e916
Melvin <yehudamelvin01@gmail.com>
2024-05-24 19:19:40 +0700

    Edit Melvin

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ barang = [
 ] 
 
 def landing():
-    print("Selamat Datang Di Aplikasi Inventaris Gudang!!")
+    print("====== Selamat Datang Di Aplikasi Inventaris Gudang!! =====")
+    print("")
     while True:
         print("Anda Ingin Masuk Sebagai : ")
         print("1. Admin")
         print("2. User")
+        print("")
 
         try :
             pilihan = int(input("Masukkan Pilihan Anda: "))
+            print("")
             if pilihan == 1:
                 login()
                 break
@@ -28,8 +31,10 @@ def landing():
                 break
             else:
                 print("Silahkan Pilih Sesuai Yang Tertera")
+                print("")
         except(ValueError):
             print("Masukan Angka Sesuai Pilihan yang tertera")
+            print("")
         
     
 
@@ -42,88 +47,114 @@ def check_user_password(username, password):
 
 # Function to handle the login process
 def login():
-    print("Welcome to the login system!")
+    print("=====Welcome to the login system!=====")
     username = input("Enter your username: ")
     password = input("Enter your password: ")
+    print("")
 
     # Check if the user exists and the password is correct
     if check_user_password(username, password):
         print("Login successful!")
         print("Welcome Admin!!")
+        print("")
         menu_admin()
     else:
         print("Invalid username or password. Please try again.")
 
 def menu_admin():
+    print("")
     print("========== MENU ADMIN ==========")
     print("1. Lihat Barang")
     print("2. Tambah Barang")
-    print("3. Edit Barang")
-    print("4. Cari Barang")
+    print("3. Edit Nama Barang Barang")
+    print("4. Cari Barang Menurut Kode")
     print("5. Hapus Barang")
     print("6. Keluar")
     pil_menu = int(input("Masukan Angka Pilihan Yang Tertera : "))
+    print("")
 
     if pil_menu == 1 :
         lihatmenuAd(barang)
     elif pil_menu == 2 :
-        print("Tambah Menu")
         tambahmenuAd(barang)
     elif pil_menu == 3 :
-        print("Edit Menu")
+        editmenuAd(barang)
     elif pil_menu == 4 :
         carimenuAd(barang)
     elif pil_menu == 5 :
         hapusmenuAd(barang)
     elif pil_menu == 6 : 
-        print("========== TERIMA KASIH ==========")
-        exit
+        keluarAd()
     else:
         print("Pilihan Invalid, Mohon Input Ulang")
+        print("")
         menu_admin()
 
 def menu_user():
+    print("")
     print("========== MENU USER ==========")
     print("1. Lihat Barang")
     print("2. Cari Barang")
     print("3. Keluar") 
     pil_menus = int(input("Masukan Angka Pilihan Yang Tertera : "))
+    print("")
 
     if pil_menus == 1:
         lihatmenuUs(barang)
     elif pil_menus == 2:
         carimenuUs(barang)
     elif pil_menus == 3:
-        print("========== TERIMA KASIH ==========")
-        exit
+        keluarUs()
     else:
         print("Pilihan Invalid, Mohon Input Ulang")
         menu_user()
 
 def tambahmenuAd(barang):
     jumlah = int(input("Berapa Barang yang ingin ditambahkan: "))
+    print("")
     while jumlah > 0:
         kode_barang = input("Masukkan kode barang: ")
         nama = input("Masukkan Nama barang: ")
         barang.append({"Nama": nama, "kode": kode_barang})
         jumlah -= 1
     print(f"Berhasil menambahkan barang ke daftar barang.")
+    print("")
     menu_admin()
 
 def lihatmenuAd(barang):
     print(f"Data Barang: {barang}")
+    print("")
     menu_admin()
 
 def carimenuAd(barang):
-    search_value = input("Masukkan nam/kode barang yang ingin Anda cari: ")
+    search_value = input("Masukkan kode barang yang ingin Anda cari: ")
+    print("")
     found = False
     for item in barang:
         if item['Nama'] == search_value or item['kode'] == search_value:
-            print(f"Barang dengan kode '{search_value}' yang dicari ditemukan dengan nama '{item['Nama']}'")
+            print(f"Barang dengan kode '{search_value}' memunculkan barang '{item['Nama']}'")
+            print("")
             found = True
             break
     if not found:
-        print(f"Barang dengan nam/kode '{search_value}' tidak ditemukan.")
+        print(f"Barang dengan kode '{search_value}' tidak ditemukan.")
+        print("")
+    menu_admin()
+
+def editmenuAd(barang):
+    kode_barang = input("Masukkan kode barang yang ingin diedit: ")
+    print("")
+    found = False
+    for item in barang:
+        if item['kode'] == kode_barang:
+            print(f"Barang dengan kode {kode_barang} ditemukan.")
+            nama_barang = input("Masukkan nama baru untuk barang: ")
+            item['Nama'] = nama_barang
+            print(f"Nama barang berhasil diubah menjadi {nama_barang}.")
+            found = True
+            break
+    if not found:
+        print(f"Barang dengan kode {kode_barang} tidak ditemukan. Silakan coba lagi.")
     menu_admin()
 
 def hapusmenuAd(barang):
@@ -133,15 +164,24 @@ def hapusmenuAd(barang):
         found = False
         for item in barang:
             if item['kode'] == kode_barang:
+                if not item['Nama']:
+                    print("Barang sudah habis, mohon kembali ke menu dan tambahkan barang")
+                    menu_admin()
+                    break
                 barang.remove(item)
                 print(f"Barang dengan kode {kode_barang} telah dihapus.")
                 found = True
                 break
         if not found:
             print(f"Barang dengan kode {kode_barang} tidak ditemukan. Silakan coba lagi.")
+            break
         else:
             break
+    if not barang:
+        print("Barang sudah habis, mohon kembali ke menu dan tambahkan barang")
+        menu_admin()
     print(f"Data barang: {barang}")
+    print("")
     menu_admin()
 
 
@@ -150,13 +190,39 @@ def lihatmenuUs(barang):
     menu_user()
 
 def carimenuUs(barang):
-    search_value = input("Masukkan nam/kode barang yang ingin Anda cari: ")
-    if search_value in barang.values():
-        print(f"Barang '{search_value}' ditemukan dengan kode '{next(key for key, value in barang.items() if value == search_value)}'.")
-    else:
-        print(f"Barang dengan nama '{search_value}' tidak ditemukan.")
+    search_value = input("Masukkan kode barang yang ingin Anda cari: ")
+    found = False
+    for item in barang:
+        if item['Nama'] == search_value or item['kode'] == search_value:
+            print(f"Barang dengan kode '{search_value}' memunculkan barang '{item['Nama']}'")
+            found = True
+            break
+    if not found:
+        print(f"Barang dengan kode '{search_value}' tidak ditemukan.")
     menu_user()
 
+def keluarAd():
+    keluar = input("Apakah anda yakin ingin keluar dari aplikasi? (Y/N): ")
+    if keluar == "Y" or keluar == "y":
+        print("========== TERIMA KASIH ==========")
+        exit
+    elif keluar == "N" or keluar == "n":
+        menu_admin()
+    else:
+        print("Invalid")
+        print("")
+        keluarAd()
 
-# Call the login function to start the login process
+def keluarUs():
+    keluar = input("Apakah anda yakin ingin keluar dari aplikasi? (Y/N): ")
+    if keluar == "Y" or keluar == "y":
+        print("========== TERIMA KASIH ==========")
+        exit
+    elif keluar == "N" or keluar == "n":
+        menu_user()
+    else:
+        print("Invalid")
+        print("")
+        keluarUs()
+     
 landing()
